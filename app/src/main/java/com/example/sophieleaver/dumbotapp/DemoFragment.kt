0e52d454865f1916805f1150cf45df1256c03b31
sophieleaver <s.e.leaver@gmail.com>
2019-01-30 19:38:26 +0000

    simple on/off motor firebase function on the demo page

--- app/src/main/java/com/example/sophieleaver/dumbotapp/DemoFragment.kt
+++ app/src/main/java/com/example/sophieleaver/dumbotapp/DemoFragment.kt
@@ -4,13 +4,16 @@ import android.content.Context
 import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.fragment_demo_fragment.view.*
 
-
-//private const val ARG_PARAM1 = "param1"
-//private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -18,30 +21,40 @@ import android.view.ViewGroup
  * Contains simple buttons to control the robot.
  *
  */
-class DemoFragment : Fragment() {
-//    private var param1: String? = null
-//    private var param2: String? = null
+class DemoFragment : Fragment(), View.OnClickListener {
+    val demoTag = "DemoFragment"
+    val database = FirebaseDatabase.getInstance()
+    val ref = database.getReference()
+    val motorStatus : DatabaseReference = ref.child("motor").child("status")
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-//            param1 = it.getString(ARG_PARAM1)
-//            param2 = it.getString(ARG_PARAM2)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle? ): View? {
+        val view = inflater.inflate(R.layout.fragment_demo_fragment, container, false)
+        val b1 : Button = view!!.findViewById(R.id.button_start_motor)
+        val b2 : Button = view!!.findViewById(R.id.button_stop_motor)
+        b1.setOnClickListener(this)
+        b2.setOnClickListener(this)
+        // Inflate the layout for this fragment
+
+        return view
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id){
+            R.id.button_start_motor -> {
+                motorStatus.setValue("ON")
+                Log.d(demoTag,"Motor status set to start")
+            }
+            R.id.button_stop_motor -> {
+                motorStatus.setValue("OFF")
+            }
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_demo_fragment, container, false)
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    fun onButtonPressed(uri: Uri) {
-    }
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
