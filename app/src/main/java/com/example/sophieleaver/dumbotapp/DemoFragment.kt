c35fdd58fcdca0c1ecd4a7269754ab68ae4b0cf1
Daniel Akinola <dakinola892@gmail.com>
2019-02-04 04:33:30 +0000

    attempt to set up weight fragment and some small codebase clean up

--- app/src/main/java/com/example/sophieleaver/dumbotapp/DemoFragment.kt
+++ app/src/main/java/com/example/sophieleaver/dumbotapp/DemoFragment.kt
@@ -1,7 +1,5 @@
 package com.example.sophieleaver.dumbotapp
 
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.util.Log
@@ -9,10 +7,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-import kotlinx.android.synthetic.main.fragment_demo_fragment.view.*
 
 
 /**
@@ -22,15 +18,11 @@ import kotlinx.android.synthetic.main.fragment_demo_fragment.view.*
  *
  */
 class DemoFragment : Fragment(), View.OnClickListener {
-    val demoTag = "DemoFragment"
-    val database = FirebaseDatabase.getInstance()
-    val ref = database.getReference()
-    val motorStatus : DatabaseReference = ref.child("motor").child("status")
+    private val demoTag = "DemoFragment"
+    private val database = FirebaseDatabase.getInstance()
+    private val ref = database.reference
+    private val motorStatus : DatabaseReference = ref.child("motor").child("status")
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle? ): View? {
         // inflate layout for demo fragment
@@ -38,7 +30,7 @@ class DemoFragment : Fragment(), View.OnClickListener {
 
         val b1 : Button = view!!.findViewById(R.id.button_start_motor)
         b1.setOnClickListener(this)
-        val b2 : Button = view!!.findViewById(R.id.button_stop_motor)
+        val b2 : Button = view.findViewById(R.id.button_stop_motor)
         b2.setOnClickListener(this)
 
         return view
@@ -57,16 +49,6 @@ class DemoFragment : Fragment(), View.OnClickListener {
             }
         }
     }
-
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-    }
-
-    override fun onDetach() {
-        super.onDetach()
-    }
-
 
     companion object {
         @JvmStatic
--- app/src/main/java/com/example/sophieleaver/dumbotapp/MainActivity.kt
+++ app/src/main/java/com/example/sophieleaver/dumbotapp/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.sophieleaver.dumbotapp
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
@@ -46,9 +45,9 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        when (item.itemId) {
-            R.id.action_settings -> return true
-            else -> return super.onOptionsItemSelected(item)
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
         }
     }
 
